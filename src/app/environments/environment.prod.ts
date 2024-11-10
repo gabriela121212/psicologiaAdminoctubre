7f0cb2c5387dac35ba7d0b03d02da7549f0688c3
Arianna0206 <bananafish@outlook.es>
2024-11-09 22:54:44 -0500

    conexiones

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCnq60wXc293HSmmiofwnwlS0WFADKFYAk',
+    authDomain: 'psicologia-app-liid.firebaseapp.com',
+    projectId: 'psicologia-app-liid',
+    storageBucket: 'psicologia-app-liid.firebasestorage.app',
+    messagingSenderId: '758525324097',
+    appId: '1:758525324097:web:81c06fd357901ab00a8fc0',
+    measurementId: 'G-MBVCXZ4K0W',
+  },
+};
